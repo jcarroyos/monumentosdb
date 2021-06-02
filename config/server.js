cc1d8963f532d7cbda89b7eb623159c1f0020c15
Juan Carlos Arroyo Sosa <juan.arroyo@comisiondelaverdad.co>
2021-06-01 19:06:39 -0500

    instalación del strapi en local

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '35c4cf290300bb0680a35f0ae40bfb3e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9c8b487d-5401-4293-89bd-39be8e6827c9'
+};
