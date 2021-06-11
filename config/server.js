7b93638d71a684b6ed7d75c12f84c4558ad60c00
jucmurciaga <jucmurciaga@unal.edu.co>
2021-06-11 15:34:06 -0500

    actualizar ruta server heroku

--- config/server.js
+++ config/server.js
@@ -1,9 +1,4 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', 'https://monumentosdb.herokuapp.com'),
+  host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '35c4cf290300bb0680a35f0ae40bfb3e'),
-    },
-  },
 });
