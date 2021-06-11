33ff6d4b071622484d4ea4917470444000a47a5b
jucmurciaga <jucmurciaga@unal.edu.co>
2021-06-11 13:06:27 -0500

    Revert "primer monumento creado"
    This reverts commit fc631fde8d3590994ac66ba1b6c4467f506c4e56.

--- config/database.js
+++ config/database.js
@@ -2,17 +2,14 @@ module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
-      connector: 'mongoose',
+      connector: 'bookshelf',
       settings: {
-        uri: env('DATABASE_URI'),
-        srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME'),
+        client: 'sqlite',
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', true),
+        useNullAsDefault: true,
       },
     },
   },
-})
+});
